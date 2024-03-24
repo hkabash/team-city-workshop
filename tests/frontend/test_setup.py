9e09a5d45d1bbeed91f3e92829f987440f4f8b62
hmaniak19 <hanna.maniak19@gmail.com>
2024-03-24 12:56:50 +0300

    add installing allure always

--- tests/frontend/test_setup.py
+++ tests/frontend/test_setup.py
@@ -5,12 +5,15 @@ from pages.favorite_projects_page import FavoriteProjectsPage
 from pages.setup_page import SetupPage
 
 
+@allure.title("Сетап Teamcity перед всеми тестами")
 def test_setup(browser_for_setup):
-    with allure.step("Сетап"):
+    with allure.step("Сетап тимсити: создание БД, принятие пользовательского соглашения"):
         setup_page = SetupPage(browser_for_setup)
         setup_page.setup()
+    with allure.step("Проверка редиректа на Home Page после логина"):
         home_page = FavoriteProjectsPage(browser_for_setup)
         home_page.check_favorite_projects_url()
         home_page.header.check_user_avatar_visible()
+    with allure.step("Авторизация агента"):
         agents_page = AgentsPage(browser_for_setup)
         agents_page.authorize_agent()
